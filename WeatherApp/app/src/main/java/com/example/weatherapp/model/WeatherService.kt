8752b718c06ff5904fb5fb42e68a7593ce16f909
Beatrice Poriazov <bettypori@gmail.com>
2024-11-19 16:09:14 +0100

    Continue on start

--- WeatherApp/app/src/main/java/com/example/weatherapp/model/WeatherService.kt
+++ WeatherApp/app/src/main/java/com/example/weatherapp/model/WeatherService.kt
@@ -1,4 +1,20 @@
 package com.example.weatherapp.model
 
+import com.example.weatherapp.data.WeatherData
+import com.example.weatherapp.data.WeatherRepository
+
 class WeatherService {
+    private val weatherRepository = WeatherRepository()
+    //private val coordinatesRepository = CoordinatesRepository()
+
+    private var _weatherData: List<WeatherData> = listOf()
+    val weatherData: List<WeatherData> = _weatherData
+
+    private fun fetchWeather(lonLat: String) {
+        weatherRepository.fetchWeather(lonLat) { processedData ->
+            _weatherData = processedData }
+    }
+
+
+    // metod som fixar time och day
 }
