891f660fad55bd911d5f328020058f4ed67456cb
Olivia Hultman <olivoljahultman@gmail.com>
2024-11-20 20:11:54 +0100

    Påbörjat databasen för väderdata

--- WeatherApp/app/src/main/java/com/example/weatherapp/data/WeatherDao.kt
+++ WeatherApp/app/src/main/java/com/example/weatherapp/data/WeatherDao.kt
@@ -1,4 +1,20 @@
 package com.example.weatherapp.data
 
-class WeatherDao {
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Update
+import com.example.weatherapp.model.Weather
+
+@Dao
+interface WeatherDao {
+
+    @Insert
+    suspend fun insert(weather: Weather)
+
+    @Query("SELECT * FROM weather WHERE location = :location")
+    suspend fun getWeatherByLocation(location: String): Weather
+
+    @Update
+    suspend fun update(weather: Weather)
 }
--- WeatherApp/app/src/main/java/com/example/weatherapp/data/WeatherEntity.kt
+++ WeatherApp/app/src/main/java/com/example/weatherapp/data/WeatherEntity.kt
@@ -2,18 +2,34 @@ package com.example.weatherapp.data
 
 import androidx.room.Entity
 import androidx.room.PrimaryKey
-import com.example.weatherapp.model.Location
 import com.example.weatherapp.model.WeatherDay
 import com.example.weatherapp.model.WeatherTime
+import java.time.LocalTime
 
 @Entity(tableName = "weather")
 data class WeatherEntity (
     @PrimaryKey
-    val location: Location,
+    val locality: String,
+
+    val municipality: String,
+
+    val county: String,
 
     val approvedTime: String,
 
     val weather7Days: List<WeatherDay>,
 
     val _weather24Hours: List<WeatherTime> = emptyList()
+)
+
+data class WeatherDayEntity (
+    val time: LocalTime,
+    val temperature: Int,
+    val icon: Int
+)
+
+class WeatherTime (
+    val time: LocalTime,
+    val temperature: Int,
+    val icon: Int
 )
