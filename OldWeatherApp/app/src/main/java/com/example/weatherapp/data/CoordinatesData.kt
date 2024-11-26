44498811bbdaa3cd4fe2553916849813cb79421f
Olivia Hultman <olivoljahultman@gmail.com>
2024-11-26 16:36:59 +0100

    Tagit bort OldWeather

--- OldWeatherApp/app/src/main/java/com/example/weatherapp/data/CoordinatesData.kt
+++
@@ -1,6 +0,0 @@
-package com.example.weatherapp.data
-
-class CoordinatesData (
-    val lon: Double?,
-    val lat: Double?
-)
--- OldWeatherApp/app/src/main/java/com/example/weatherapp/data/WeatherData.kt
+++
@@ -1,6 +0,0 @@
-package com.example.weatherapp.data
-
-data class WeatherData (
-    val approvedTime: String?,
-    val timeData: List<WeatherTimeData>?
-)
