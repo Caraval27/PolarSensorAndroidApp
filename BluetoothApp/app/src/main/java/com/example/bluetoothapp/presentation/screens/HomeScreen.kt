469465d91f1f772a6ac80e3d40f7605053102ab7
Caraval27 <bettypori@gmail.com>
2024-11-28 17:10:23 +0100

    Simple HomeScreen and created a DeviceScan component

--- BluetoothApp/app/src/main/java/com/example/bluetoothapp/presentation/screens/HomeScreen.kt
+++ BluetoothApp/app/src/main/java/com/example/bluetoothapp/presentation/screens/HomeScreen.kt
@@ -1,24 +1,15 @@
 package com.example.bluetoothapp.presentation.screens
 
-import android.os.Build
 import androidx.activity.result.ActivityResultLauncher
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Button
-import androidx.compose.material3.Card
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -26,7 +17,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-import com.example.bluetoothapp.domain.Device
+import com.example.bluetoothapp.presentation.components.DeviceScan
 import com.example.bluetoothapp.presentation.viewModel.MeasurementVM
 
 @Composable
@@ -34,106 +25,48 @@ fun HomeScreen(
     requestPermissionLauncher: ActivityResultLauncher<Array<String>>,
     measurementVM: MeasurementVM
 ) {
-    val devices by measurementVM.devices.collectAsState()
-    var isScanning by remember { mutableStateOf(false) }
-    var permissionDenied by remember { mutableStateOf(false) }
+    var polarSelected by remember { mutableStateOf(false) }
 
+    //Knappnamnen kan ändras
     //Knapp för history ska finnas, när man trycker ska den hämta history i vm
+    //Knapp för internal sensor --> startar skanning direkt?
+    //Knapp för bluetooth --> innehållet i home ändras till att scanna efter enheter
 
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(16.dp),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        if (permissionDenied) {
-            Text("Permissions are required to scan for devices.")
-        }
-
-        Button(onClick = {
-            if (isScanning) {
-                isScanning = false
-            } else if (measurementVM.hasRequiredPermissions()) {
-                isScanning = true
-                measurementVM.searchForDevices()
-            } else {
-                requestPermissionLauncher.launch(
-                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
-                        arrayOf(
-                            android.Manifest.permission.BLUETOOTH_SCAN,
-                            android.Manifest.permission.BLUETOOTH_CONNECT
-                        )
-                    } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
-                        arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION)
-                    } else {
-                        arrayOf(android.Manifest.permission.ACCESS_COARSE_LOCATION)
-                    }
-                )
-            }
-        }) {
-            Text(if (isScanning) "Searching..." else "Search for Devices")
-        }
-
-        Spacer(modifier = Modifier.height(16.dp))
-
-        if (devices.isNotEmpty()) {
-            Text(
-                "Select a Device",
-                style = MaterialTheme.typography.titleMedium,
-                modifier = Modifier.padding(bottom = 16.dp)
-            )
-            LazyColumn {
-                items(devices) { device ->
-                    DeviceItem(device = device, onSelect = { selectedDevice ->
-                        measurementVM.connectToDevice(selectedDevice.deviceId)
-                    })
-                }
-            }
-        } else if (!isScanning) {
-            Text("No devices found. Press search to start scanning.")
-        }
-    }
-}
-
-@Composable
-fun DeviceItem(device: Device, onSelect: (Device) -> Unit) {
-    Card(
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(vertical = 8.dp)
-            .clickable { onSelect(device) },
-    ) {
-        Row(
-            modifier = Modifier.padding(16.dp),
-            verticalAlignment = Alignment.CenterVertically
+    if (!polarSelected) {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            Column {
-                Text(text = device.name ?: "Unknown Device", style = MaterialTheme.typography.displayMedium)
-                Text(text = "ID: ${device.deviceId}", style = MaterialTheme.typography.bodyMedium)
+            Button(
+                onClick = { TODO("navcontroller to PlotScreen") },
+                modifier = Modifier.fillMaxWidth(0.5f)
+            ) {
+                Text("Use internal sensor")
+            }
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            Button(
+                onClick = { polarSelected = true },
+                modifier = Modifier.fillMaxWidth(0.5f)
+            ) {
+                Text("Use polar sensors")
+            }
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            Button(
+                onClick = { TODO("navcontroller to MeasurementHistoryScreen") },
+                modifier = Modifier.fillMaxWidth(0.5f)
+            ) {
+                Text("Result history")
             }
         }
+    } else {
+        DeviceScan(requestPermissionLauncher, measurementVM)
+        /*TODO("Ska även skicka in navcontroller för att kunna gå vidare till PlotScreen")*/
     }
 }
-
-/*if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
-            activity.registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) { permissions ->
-                onPermissionsResult(
-                    permissions[Manifest.permission.BLUETOOTH_SCAN] == true &&
-                            permissions[Manifest.permission.BLUETOOTH_CONNECT] == true
-                )
-            }.launch(
-                arrayOf(
-                    Manifest.permission.BLUETOOTH_SCAN,
-                    Manifest.permission.BLUETOOTH_CONNECT
-                )
-            )
-        } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
-            activity.registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted ->
-                onPermissionsResult(isGranted)
-            }.launch(Manifest.permission.ACCESS_FINE_LOCATION)
-        } else {
-            activity.registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted ->
-                onPermissionsResult(isGranted)
-            }.launch(Manifest.permission.ACCESS_COARSE_LOCATION)
-        }*/
