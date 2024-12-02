401f7b190687c9dfd58b49905f8a2e194aa1f752
Caraval27 <bettypori@gmail.com>
2024-12-02 21:35:00 +0100

    Fix

--- BluetoothApp/app/src/main/java/com/example/bluetoothapp/presentation/components/setupLineChart.kt
+++ BluetoothApp/app/src/main/java/com/example/bluetoothapp/presentation/components/setupLineChart.kt
@@ -67,7 +67,7 @@ fun setupLineChart(
         axisLeft.apply {
             textColor = Color.WHITE
             setDrawGridLines(true)
-            axisMinimum = -10f // Set the predefined minimum Y value
+            axisMinimum = -10f 
             axisMaximum = 100f
         }
 
