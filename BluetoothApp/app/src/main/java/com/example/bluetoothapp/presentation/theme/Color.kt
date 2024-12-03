2f511e6ef0ecdcb49e4b192f6b498003610c491d
Caraval27 <bettypori@gmail.com>
2024-12-03 18:24:56 +0100

    Scan for devices fix

--- BluetoothApp/app/src/main/java/com/example/bluetoothapp/presentation/theme/Color.kt
+++ BluetoothApp/app/src/main/java/com/example/bluetoothapp/presentation/theme/Color.kt
@@ -11,5 +11,5 @@ val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
 
 val BluePrimary = Color(169, 114, 218, 255)
-val LightBlueSecondary = Color(176, 196, 222)
+val LightBlueSecondary = Color(169, 114, 218, 77)
 val LightGrayBackground = Color(240, 240, 240)
