d23a2b1e6fb8bb778e2a21b4e3d6d6c16b796fa7
Caraval27 <bettypori@gmail.com>
2025-01-16 12:21:49 +0100

    Deleted WeatherApp

--- WeatherApp/app/src/main/java/com/example/weatherapp/data/CoordinatesData.kt
+++
@@ -1,9 +0,0 @@
-package com.example.weatherapp.data
-
-import com.example.weatherapp.model.Location
-
-class CoordinatesData (
-    val lon: Double,
-    val lat: Double,
-    val location: Location
-)
--- WeatherApp/app/src/main/java/com/example/weatherapp/data/CoordinatesResponse.kt
+++
@@ -1,9 +0,0 @@
-package com.example.weatherapp.data
-
-data class CoordinatesResponse(
-    val lon: Double,
-    val lat: Double,
-    val place: String,
-    val municipality: String,
-    val county: String
-)
